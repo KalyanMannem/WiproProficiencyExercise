ce57b269296ed345840e9a77ae78f4676d79c99b
Kalyan <kmannem2@gmail.com>
2019-12-12 13:44:45 +0530

    AppConstants Created for adding constants throughout the app.

---
+++ WiproProficiencyExercise/Helpers/AppConstants.swift
@@ -0,0 +1,14 @@
+//
+//  AppConstants.swift
+//  WiproProficiencyExercise
+//
+//  Created by Kalyan Mannem on 12/12/19.
+//  Copyright © 2019 CompIndia. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants
+{
+    static let SERVICE_URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+}
