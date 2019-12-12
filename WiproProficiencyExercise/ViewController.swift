845b2510fdd40c42f913e20d398a0dfd0036d0f3
Kalyan <kmannem2@gmail.com>
2019-12-12 13:27:27 +0530

    TableView Creation

--- WiproProficiencyExercise/ViewController.swift
+++ WiproProficiencyExercise/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
+    let tableView = UITableView()
+    let directions = ["East", "West", "North", "South"]
+    
+    override func loadView()
+    {
+        super.loadView()
+        setUpTableView()
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        tableView.dataSource = self
+    }
+    
+    func setUpTableView()
+    {
+        let safeGuide = self.view.safeAreaLayoutGuide
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: safeGuide.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: safeGuide.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: safeGuide.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: safeGuide.rightAnchor).isActive = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
+}
+
+extension ViewController: UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return directions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel!.text = directions[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
