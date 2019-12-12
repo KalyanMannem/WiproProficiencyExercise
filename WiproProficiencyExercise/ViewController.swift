4249dbff5ad3df321948385e2230f4291372200c
Kalyan <kmannem2@gmail.com>
2019-12-12 12:36:07 +0530

    Initial Commit

---
+++ WiproProficiencyExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WiproProficiencyExercise
+//
+//  Created by Kalyan Mannem on 12/12/19.
+//  Copyright © 2019 CompIndia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
